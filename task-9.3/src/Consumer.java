87ac4d832b7e5466663935022364ad22180ded00
4epic <AndreyNovitski@mail.ru>
2021-03-26 19:04:43 +0300

    task-9.3

---
+++ task-9.3/src/Consumer.java
@@ -0,0 +1,32 @@
+import java.util.Queue;
+
+class Consumer implements Runnable {
+
+    private final Queue<Double> sharedQueue;
+
+    public Consumer(Queue<Double> sharedQueue) {
+        this.sharedQueue = sharedQueue;
+    }
+
+    @Override
+    public void run() {
+            while (true) {
+                try {
+                    System.out.println("Consumed: " + consume());
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+    }
+
+    private Double consume() throws InterruptedException {
+        synchronized (sharedQueue) {
+            if (sharedQueue.isEmpty()) {
+                sharedQueue.wait();
+            }
+
+            sharedQueue.notifyAll();
+            return sharedQueue.poll();
+        }
+    }
+}
